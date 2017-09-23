ad32a730e957ee9a620ad916c3783fd19c111b7c
Connor Ness <connordvn@gmail.com>
2017-09-22 23:56:42 -0500

    A2: moved item arrays, finished displayArray method with formatting

--- assignments/2/Ness_Connor_A2.java
+++ assignments/2/Ness_Connor_A2.java
@@ -4,14 +4,26 @@ Author:     Connor Ness
 Date:       September 24, 2017
 */
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Ness_Connor_A2 {
-    public static void main(String[] args){
 
+    private static String[] books = {"Intro to Java", "Intro to C++", "Python",
+            "Perl", "C#"};
+    private static double[] booksprices = {45.99, 89.34, 100.00, 25.00, 49.99};
+
+    private static String[] dvds = {"Snow White", "Cinderella", "Dumbo",
+            "Bambi", "Frozen"};
+    private static double[] dvdsPrices = {19.99, 24.99, 17.99, 21.99, 24.99};
+
+    // private String[] cartItems = {};
+
+    public static void main(String[] args){
+        menu();
     }
 
-    public static void menu() {
+    private static void menu() {
 
         Scanner input = new Scanner(System.in); // setup input
         System.out.printf(
@@ -29,9 +41,9 @@ public class Ness_Connor_A2 {
         );
 
         switch(input.nextInt()) {
-            case 1: // browse books
+            case 1: displayArrays(books, booksprices, "Books");
                 menu();
-            case 2: // browse DVDs
+            case 2: displayArrays(dvds, dvdsPrices, "DVDs");
                 menu();
             case 3: // add book
                 menu();
@@ -43,14 +55,66 @@ public class Ness_Connor_A2 {
                 menu();
             case 7: // cancel order
                 menu();
-            case 8: return;
+            case 8: break;
             default:
                 System.out.println(
                         "Invalid input! Please use an integer from 1-8."
                 );
                 menu();
         }
+    }
 
+    private static void displayArrays(String[] itemsArray, double[] pricesArray,
+                                      String itemType) {
 
+        // format the data to be readable:
+        // %[index$][flags][width][.precision]conversion
+        System.out.printf(
+                "Inventory Number    %1$5s                  Prices%n" +
+                "-------------------------------------------------%n",
+        itemType);
+
+        int[] invNum = new int[itemsArray.length];
+        for(int i = 0; i < invNum.length; i++) {
+            invNum[i] = i + 1;
+        }
+
+        // sort arrays by price, low to high using selection sort
+        // TODO: more comments to describe swaps to three arrays
+        int minIndex, minInvNum;
+        String minTitle;
+        double minValue;
+
+        for (int scan = 0; scan < (itemsArray.length - 1); scan++) {
+            minIndex = scan;
+            minInvNum = invNum[scan];
+            minTitle = itemsArray[scan];
+            minValue = pricesArray[scan];
+
+            for (int i = scan + 1; i < invNum.length; i++) {
+                if (pricesArray[i] < minValue) {
+                    minInvNum = invNum[i];
+                    minTitle = itemsArray[i];
+                    minValue = pricesArray[i];
+                    minIndex = i;
+                }
+            }
+
+            invNum[minIndex] = invNum[scan];
+            itemsArray[minIndex] = itemsArray[scan];
+            pricesArray[minIndex] = pricesArray[scan];
+
+            invNum[scan] = minInvNum;
+            itemsArray[scan] = minTitle;
+            pricesArray[scan] = minValue;
+        }
+
+        DecimalFormat df = new DecimalFormat("#.00");
+        for (int x = 0; x < itemsArray.length; x++) {
+            String price = "$" + df.format(pricesArray[x]);
+            System.out.printf("%-20d%-16s%13s%n", invNum[x], itemsArray[x],
+                    price);
+        }
+        System.out.println();
     }
 }
