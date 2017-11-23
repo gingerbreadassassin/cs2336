029d192beeec898754d3c7634296bc75499883c8
Connor Ness <connordvn@gmail.com>
2017-11-23 01:48:16 -0600

    begin A4

--- assignments/4/Ness_Connor_A4.java
+++ assignments/4/Ness_Connor_A4.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class Ness_Connor_A4 {
     private static Scanner input = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        System.out.println("Please input a string of characters: ");
+        String chars = getStringInput();
+    }
+
 
 
 //    getStringInput gets input from the console. It makes sure that the
